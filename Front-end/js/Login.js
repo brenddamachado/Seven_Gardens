c1ef73296eddc1857d6a25ae2d88a7daed8ea5ca
Taisbarradas <taisbarradassilva@gmail.com>
2024-03-24 22:46:22 -0300

    formulario login

---
+++ Front-end/js/Login.js
@@ -0,0 +1,15 @@
+validaLogin = function () {
+    const usuario = document.getElementById("login").value.toLowerCase();
+    const senha = document.getElementById("senha").value;
+
+    const objetoUsuario = localStorage.getItem(usuario);
+    const usuarioEncontrado = JSON.parse(objetoUsuario);
+        if (usuarioEncontrado != null && usuarioEncontrado.senha === senha) {
+            usuarioEncontrado.isLogged = true;
+            const usuarioEncontradoString = JSON.stringify(usuarioEncontrado);
+            localStorage.setItem(usuario, usuarioEncontradoString);
+            location.href = '#';
+        } else {
+            alert('Usuário ou senha incorretos');
+        }
+}
