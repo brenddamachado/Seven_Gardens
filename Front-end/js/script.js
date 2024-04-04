4cd5eaf86ced27175562dd272c1bb8d0a8ec7a19
Nathaliaaguiar <59691090+Nathaliaaguiar@users.noreply.github.com>
2024-04-04 14:54:58 -0300

    Update script.js
    ajustes

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -139,3 +139,42 @@ function usuarioLogado() {
   return true; // Exemplo: usuário sempre está logado
 }
 // Fim do Carrinho de compras
+
+
+//incio do produtos 
+
+document.addEventListener("DOMContentLoaded", function() {
+  var produtos = document.getElementById("produtos");
+  var categorias = document.querySelector(".categorias");
+  var listaCategorias = document.querySelectorAll(".categoria");
+
+  // Quando o mouse entra nos produtos ou nas categorias, mantenha as categorias visíveis
+  produtos.addEventListener("mouseenter", function() {
+    categorias.style.display = "block";
+  });
+
+  categorias.addEventListener("mouseenter", function() {
+    categorias.style.display = "block";
+  });
+
+  // Quando o mouse sai dos produtos ou das categorias, esconda as categorias
+  produtos.addEventListener("mouseleave", function() {
+    categorias.style.display = "none";
+  });
+
+  categorias.addEventListener("mouseleave", function() {
+    categorias.style.display = "none";
+  });
+
+  // Adicione o evento de clique para selecionar uma categoria
+  listaCategorias.forEach(function(categoria) {
+    categoria.addEventListener("click", function() {
+      // Remova a classe 'selecionada' de todas as categorias
+      listaCategorias.forEach(function(c) {
+        c.classList.remove("selecionada");
+      });
+      // Adicione a classe 'selecionada' apenas à categoria clicada
+      categoria.classList.add("selecionada");
+    });
+  });
+});
