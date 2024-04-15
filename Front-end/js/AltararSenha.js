6b5cca8c7814f8e445bc1fb37a29392bde6ed00b
Felipe Souza <fn23886@gmail.com>
2024-04-15 20:28:37 -0300

    feat: dark-mode

--- Front-end/js/AltararSenha.js
+++ Front-end/js/AltararSenha.js
@@ -37,4 +37,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+  let hamburger = document.getElementById('hamburguer');
+  let mobileMenu = document.getElementById('mobile');
+  let closeButton = document.querySelector('.close-btn');
   
+  hamburger.addEventListener('click', function() {
+    mobileMenu.style.left = "0"; // Abre o menu
+  });
+  
+  closeButton.addEventListener('click', function() {
+    mobileMenu.style.left = "-100%"; // Fecha o menu
+  });
+    
