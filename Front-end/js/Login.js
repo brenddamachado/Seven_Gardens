08274a059bd4b519c4111c31e24cde2017ad564b
matheusbourguignon <56460651+matheusbourguignon@users.noreply.github.com>
2024-04-24 11:33:19 -0300

    Organizando a responsabilidade

--- Front-end/js/Login.js
+++ Front-end/js/Login.js
@@ -20,3 +20,16 @@ function limparCampos() {
     email.value = "";
     senha.value = "";
   }
+
+// hamburger  
+let hamburger = document.getElementById("hamburguer");
+let mobileMenu = document.getElementById("mobile");
+let closeButton = document.querySelector(".close-btn");
+
+hamburger.addEventListener("click", function () {
+  mobileMenu.style.left = "0"; // Abre o menu
+});
+
+closeButton.addEventListener("click", function () {
+  mobileMenu.style.left = "-100%"; // Fecha o menu
+});
