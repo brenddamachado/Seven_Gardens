ba78a617e402ac0f34de8c424c4db14d0668271d
brenddamachado <brenda16machado.bm@gmail.com>
2024-05-01 19:11:57 -0300

    ajustes

--- Front-end/js/Usuario.js
+++ Front-end/js/Usuario.js
@@ -25,15 +25,19 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-/*mobile */
-let hamburger = document.getElementById('hamburguer');
-let mobileMenu = document.getElementById('mobile');
-let closeButton = document.querySelector('.close-btn');
 
-hamburger.addEventListener('click', function() {
+// HAMBURGUER JS
+
+let hamburger = document.getElementById("hamburguer");
+let mobileMenu = document.getElementById("mobile");
+let closeButton = document.querySelector(".close-btn");
+
+hamburger.addEventListener("click", function () {
   mobileMenu.style.left = "0"; // Abre o menu
 });
 
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener("click", function () {
   mobileMenu.style.left = "-100%"; // Fecha o menu
 });
+
+
