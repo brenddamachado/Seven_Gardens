3d07ba022d557996991aa16dac455c283cca0fe6
Yume7 <yume_owned@hotmail.com>
2024-05-08 14:40:53 -0300

    atualização connect para PDO e pg de Relatorio php

--- Front-end/PHP/connect.php
+++ Front-end/PHP/connect.php
@@ -4,7 +4,12 @@ $dbname = 'sevengardens';
 $username = 'root';
 $password = '';
 
-$conn = new mysqli($host, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Redireciona para a página de erro na pasta Front-end
+    header('Location: ../Front-end/Erro.html');
+    exit;
 }
+?>
