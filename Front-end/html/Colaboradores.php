22917f4d326c17168933b264bf67a1ce857bf6cb
Felipe Souza <fn23886@gmail.com>
2024-05-24 11:06:06 -0300

    fix: conserto do css e js da página de Consulta de Colaboradores

--- Front-end/html/Colaboradores.php
+++ Front-end/html/Colaboradores.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Consulta de Colaboradores</title>
-  <link rel="stylesheet" href="../css/ConsultaAdm.css" />
+  <link rel="stylesheet" href="../css/ConsultaColaboradores.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 </head>
 
@@ -37,7 +37,7 @@
     <button type="submit">Pesquisar</button>
   </form>
 
-  <table id="tabelaColaboradores">
+  <table id="tabelaColaboradores"  class="tabelaCentral">
     <thead>
       <tr>
         <th>Nome do Colaborador</th>
@@ -80,7 +80,7 @@
       <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
     </div>
   </footer>
-
+  <script src="../js/acessibilidade.js"></script>
   <script src="../js/Colaboradores.js"></script>
 </body>
 
