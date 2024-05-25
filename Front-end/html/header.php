6e5e79133f7da4f4bd74a7b6f3756e06b2a16cd3
Yume7 <yume_owned@hotmail.com>
2024-05-24 22:41:04 -0300

    resolvendo conflitos

--- Front-end/html/header.php
+++ Front-end/html/header.php
@@ -1,27 +1,43 @@
+<?php
+session_start();
+?>
 <header>
   <section class="header">
     <div>
-      <a href="../../index.php"> <img class="logo" src="../img/logoatual.svg" alt="" srcset="" /></a>
+      <a href="../../index.php"><img class="logo" src="../img/logoatual.svg" alt="Logo Seven Gardens" /></a>
     </div>
-
     <div class="pesquisa-container">
       <input type="search" name="" id="" class="pesquisa" />
-      <div for="" class="pesquisa-icon"><i class="fas fa-search"></i></div>
+      <div class="pesquisa-icon"><i class="fas fa-search"></i></div>
     </div>
-    <nav>
+    <nav class="nav_a">
       <div class="navegacao">
-        <ul class="caminhos">
-          <li class="cadastrar"> <a href="Cadastro.php">Cadastro</a></li>
-          <li class="login"> <a href="Login.php">Login</a></li>
+        <ul>
+          <?php if (isset($_SESSION['usuario_tipo'])) : ?>
+            <li class="welcome-logout">
+              <span>
+                <?php
+                if ($_SESSION['usuario_tipo'] == 'Master' || $_SESSION['usuario_tipo'] == 'Colaborador') {
+                  echo "Olá, administrador!";
+                } else {
+                  echo "Olá, " . htmlspecialchars($_SESSION['usuario_nome']) . "!";
+                }
+                ?>
+              </span>
+              <a href="logout.php" class="logout">Logout</a>
+            </li>
+          <?php else : ?>
+            <li class="home"><a class="login" href="Front-end/html/Login.php">Login</a></li>
+            <li class="login"><a href="Front-end/html/Cadastro.php" class="cadastro">Cadastro</a></li>
+          <?php endif; ?>
         </ul>
       </div>
     </nav>
-  </section>
 
   <section class="opcoes">
     <div class="dropdown">
       <div class="dropbtn">
-        <span class="prod_dropdown">Produtos</span> <i class="fas fa-chevron-down"> </i>
+        <span class="prod_dropdown">Produtos</span> <i class="fas fa-chevron-down"></i>
       </div>
       <div class="dropdown-content">
         <a href="catalogo.php">Ver Todos</a>
@@ -31,8 +47,13 @@
         <a href="categoria.php?categoria=Insumos">Insumos</a>
       </div>
     </div>
-    <div class="menu_btn">
-      <a href="../../index.php">Home</a>
+    <div class="menu_btn"><a href="../../index.php">Home</a></div>
+    <div class="menu_btn"><a href="instrucoesCultivo.php">Instruções de Cultivo</a></div>
+    <div class="menu_btn"><a href="Contato.php">Contato</a></div>
+    <div class="menu_btn"><a href="Sobre.php">Sobre</a></div>
+    <div class="cart-icon-container">
+      <img src="../img/iconecar.svg" alt="Ícone do carrinho de compras" id="icon" onclick="exibirModalCarrinho()" />
+      <span id="cart-counter" class="cart-counter">0</span>
     </div>
     <div class="menu_btn">
       <a href="instrucoesCultivo.php">Instruções de Cultivo</a>
@@ -46,8 +67,7 @@
     <img src="../img/iconecar.svg" alt="Ícone do carrinho de compras" id="icon" />
   </section>
   <section class="section_mobile">
-    <div class="mobile_i"> <i id="hamburguer" class="fa fa-bars"></i></div>
-
+    <div class="mobile_i"><i id="hamburguer" class="fa fa-bars"></i></div>
     <section id="mobile" class="mobile">
       <div id="dentro_icon" class="dentro_icon">
         <div class="close-btn"><i class="fas fa-times"></i></div>
@@ -63,6 +83,6 @@
         <a href="Sobre.php">Sobre</a>
       </div>
     </section>
-    <img src="../img/iconecar.svg" alt="Ícone do carrinho de compras" id="icon" class="icon_mobile" />
+    <img src="../img/iconecar.svg" alt="Ícone do carrinho de compras" id="icon" class="icon_mobile" onclick="exibirModalCarrinho()" />
   </section>
 </header>
