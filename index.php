c9160e021edf2293f8d68dfc66ee128a5f5859a5
Yume7 <yume_owned@hotmail.com>
2024-05-26 14:29:46 -0300

    correções no css dos btns do header, teste do headerMaster

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ $isUserMasterOrColaborador = isset($_SESSION['usuario_tipo']) && in_array($_SESS
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Seven Gardens</title>
+  <link rel="stylesheet" href="<?php echo base_url('Front-end/css/header.css'); ?>">
   <link rel="shortcut icon" href="<?php echo base_url('Front-end/img/logoatual.svg'); ?>" type="image/x-icon" />
   <link rel="stylesheet" href="<?php echo base_url('Front-end/css/style.css'); ?>">
   <link rel="stylesheet" href="<?php echo base_url('Front-end/css/Card-produtos.css'); ?>">
