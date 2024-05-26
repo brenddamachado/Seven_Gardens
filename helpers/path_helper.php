63f021ae2696a09a56f7895c18e1642f915fc2fd
Yume7 <yume_owned@hotmail.com>
2024-05-26 19:21:20 -0300

    pasta helpers

--- helpers/path_helper.php
+++ helpers/path_helper.php
@@ -1,6 +1,6 @@
 <?php
-function base_url($path = '') {
+function base_url($path = '')
+{
     // Retorna a URL base da aplicação
     return 'http://localhost/Seven_Gardens/' . $path; // Ajuste para refletir a URL base correta
 }
-?>
